8084cb5d2584afe9d5e86f091b2dab644cb262ba
lefty313 <lewy313@gmail.com>
2012-04-30 18:18:24 +0200

    added rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Food::Application.config.secret_token = '3800a15a40b0a1b2002cc1b286b3ce1ff572546a35f841396a1554848deba1d64321989b1427f2dd4b1521aa21f3a658499278d6bc65f29091730ef6c740ed8e'
