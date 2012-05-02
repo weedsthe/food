7381f02ec838d877cf5eefb28f198b79f946f399
lefty313 <lewy313@gmail.com>
2012-05-03 00:11:02 +0200

    added mongoid

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# require 'rails/all'
+# http://mongoid.org/docs/installation/configuration.html
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie" # Uncomment this line for Rails 3.1+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -48,7 +54,7 @@ module Food
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
