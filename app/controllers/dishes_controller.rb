d518f8e4005cb0866df3d66614148fc64f00039f
Piotrek <pkurek90@gmail.com>
2012-05-08 00:20:38 +0200

    Added update and destroy actions on dishes_controller. Added spec for those actions and added view more with context to existing dishes_controller_spec

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,13 +3,18 @@ class DishesController < ApplicationController
 
   before_filter :find_category
 
+  # Talktrough:
+  # Mozna rowniez zrobic find_dish tylko nie wiem czy chcesz miec @dish na show i update
+  # Bo nie wiem czyc chesz to pozniej udostepniac na widoku
+
+  before_filter :find_dish, only: [:show, :update, :destroy]
+
   def index
     respond_with @category.dishes
   end
 
   def show
-    dish = @category.dishes.find(params[:id])
-    respond_with dish
+    respond_with @dish
   end
 
   def create
@@ -17,9 +22,23 @@ class DishesController < ApplicationController
     respond_with dish, location: category_dish_url(@category,dish)
   end
 
+  def update
+    @dish.update_attributes(params[:dish])
+    respond_with @dish, location: category_dish_url(@category, @dish)
+  end
+
+  def destroy
+    @dish.destroy
+    respond_with location: category_dishes_url(@category, @dish)
+  end
+
   private
 
     def find_category
       @category = Category.find(params[:category_id])
+    end
+
+    def find_dish
+      @dish = @category.dishes.find(params[:id])
     end
 end
--- spec/controllers/dishes_controller_spec.rb
+++ spec/controllers/dishes_controller_spec.rb
@@ -17,11 +17,58 @@ describe DishesController do
     response.body.should be_json_eql(dish.to_json)
   end 
 
-  it 'post create should create new dish assigned to correct category' do
-    category = Fabricate(:category, :dishes => [])
-    dish = Fabricate(:dish)
 
-    post :create, category_id: category.id, dish: dish.as_json, format: :json
-    response.body.should be_json_eql(dish.to_json)
+  context "new dish with valid input" do
+    it 'post create should create new dish assigned to correct category' do
+      category = Fabricate(:category, :dishes => [])
+      dish = Fabricate(:dish)
+      post :create, category_id: category.id, dish: dish.as_json, format: :json
+
+      response.body.should be_json_eql(dish.to_json)
+    end
+
+    it "should redirect" do
+      category = Fabricate(:category)
+      dish = category.dishes.first
+      post :create, category_id: category.id, dish: dish.as_json, format: :json
+
+      response.header['Location'].should include category_dish_path(dish.category, dish)
+      response.header['Content-Type'].should include 'application/json'
+    end
+
   end
+
+  context "update dish with valid input" do
+    it "should redirect" do
+      category = Fabricate(:category)
+      dish = category.dishes.first
+      dish.title = "New Uber Pizza"
+
+      xhr :put, :update, category_id: category.id, id: dish.id, dish: dish.as_json, format: :json
+      response.header['Location'].should include category_dish_path(category, dish)
+      #Railsy nie puszczaja tego tak naprawde jako json i tutaj caly problem dunno co robie zle;////
+      response.header['Content-Type'].should include 'application/json'
+    end
+
+    it "put update should update an existing dish" do
+      category = Fabricate(:category)
+      dish = category.dishes.first
+      dish.title = "New Uber Pizza"
+
+      xhr :put, :update, category_id: category.id, id: dish.id, dish: dish.as_json, format: :json
+      response.body.should be_json_eql(dish.to_json)
+    end
+  end
+
+  context "destroy dish with valid id" do
+    it "should redirect" do
+      category = Fabricate(:category)
+      dish = category.dishes.first
+
+      delete :destroy, category_id: category.id, id: dish.id, format: :json 
+      response.header['Location'].should include category_dishes_path(category, dish)
+      response.header['Content-Type'].should include 'application/json'
+    end
+  end
+
 end
